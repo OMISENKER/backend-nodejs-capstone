f61990f908781b5afcbce6ccd81d3f5bc9cabe85
dominiquealvarez8888@gmail.com <dominiquealvarez8888@gmail.com>
2024-12-10 03:07:34 +0800

    authroutes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,11 +1,16 @@
-/*jshint esversion: 8 */
+/* jshint esversion: 9 */
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
+const pinoLogger = require("./logger");
+const secondChanceItemsRoutes = require("./routes/secondChanceItemsRoutes");
+const searchRoutes = require("./routes/searchRoutes");
+const authRoutes = require("./routes/authRoutes");
+const imageRoutes = require("./routes/imageRoutes");
 const connectToDatabase = require("./models/db");
 const { loadData } = require("./util/import-mongo/index");
+
+loadData();
 
 const app = express();
 app.use("*", cors());
@@ -14,24 +19,28 @@ const port = 3060;
 // Connect to MongoDB; we just do this one time
 connectToDatabase()
   .then(() => {
-    console.log("Connected to DB");
+    pinoLogger.info("Connected to DB");
   })
   .catch((e) => console.error("Failed to connect to DB", e));
 
 app.use(express.json());
 
-// Route files
-const secondChanceRoutes = require("./routes/secondChanceItemsRoutes");
-const searchRoutes = require("./routes/searchRoutes");
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
 
 app.use(pinoHttp({ logger }));
-app.use(express.static(path.join(__dirname, "public")));
 
 // Use Routes
-app.use("/api/secondchance/items", secondChanceRoutes);
+// authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
+app.use("/api/auth", authRoutes);
+
+// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
+app.use("/api/secondchance/items", secondChanceItemsRoutes);
+
+// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use("/api/secondchance/search", searchRoutes);
+
+app.use("/images", imageRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
